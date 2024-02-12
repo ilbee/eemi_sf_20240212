2c79f949bacd08519bbcb1572785d1635bcbc922
Julien PRIGENT <julien.prigent@fnacdarty.com>
2024-02-12 13:00:57 +0100

    Add brand form type

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -11,3 +11,13 @@ G
 src/Entity/Category.php,c\8\c85245800948ffb0569f65af912d0e2d467f18f3
 T
 $src/Controller/ProductController.php,6\5\65b02b46f8d51efa74789e695aa9505440bd7425
+F
+src/Form/BrandType.php,e\0\e046261a1ef0e387978ce59910a6963a76849cc1
+R
+"src/Controller/BrandController.php,b\e\be9a8579a9b0e85dca0d9b4e204efc118cdffebf
+P
+ templates/brand/create.html.twig,c\9\c9666ca34b7d05d5584a22f83b949fce8f45386a
+O
+templates/brand/index.html.twig,7\1\71ad1398d21d5326ea4e15fcc4e728fa28fd24ec
+H
+templates/base.html.twig,f\f\ffb3f1cfd73fdec1f0c80c63d0b5bbc1fff22022
--- src/Controller/BrandController.php
+++ src/Controller/BrandController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Brand;
+use App\Form\BrandType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -13,6 +17,30 @@ class BrandController extends AbstractController
     {
         return $this->render('brand/index.html.twig', [
             'controller_name' => 'BrandController',
+        ]);
+    }
+
+    #[Route('/brand/create', name: 'app_brand_create')]
+    #[Route('/brand/edit/{brand}', name: 'app_brand_edit')]
+    public function create(
+        EntityManagerInterface $em,
+        Request $request,
+        Brand $brand = null
+    ): Response {
+        if (!$brand) {
+            $brand = new Brand();
+        }
+
+        $form = $this->createForm(BrandType::class, $brand);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($brand);
+            $em->flush();
+        }
+
+        return $this->render('brand/create.html.twig', [
+            'varForm' => $form->createView(),
         ]);
     }
 }
